22e818045da891506fd0e438190a2776cb0b0dfb
Eduardo Gabriel de Freitas <58999344+Tironsom@users.noreply.github.com>
2024-04-22 19:30:27 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { ApplicationModule, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EventosComponent} from './eventos/eventos.component'
 import { PalestranteComponent } from './palestrante/palestrante.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from '../nav/nav.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EventosComponent, PalestranteComponent, HttpClientModule ],
+  imports: [RouterOutlet, EventosComponent, PalestranteComponent, HttpClientModule, NavComponent
+   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
